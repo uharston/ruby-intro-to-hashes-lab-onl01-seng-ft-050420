c31e7251d0af57b6b145b46ceb2fc5dfa474a987
Uriah Harston <uharston@gmail.com>
2020-05-08 13:57:43 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,7 +12,8 @@ def monopoly
 end
 
 def monopoly_with_second_tier
-	railroads: {pieces: 4, names: {}, rent_in_dollars: {}
+	monopoly = {railroads: {pieces: 4, names: {}, rent_in_dollars: {}
+}
 }
 
 
